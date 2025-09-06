ac9e50861b925fbb42fa2d41a50220ee05132f8a
atharkn7 <athar.kn7@gmail.com>
2025-09-06 14:41:21 +0530

    Error pages

--- app.py
+++ app.py
@@ -8,4 +8,22 @@ app = Flask(__name__)
 # Main page
 @app.route("/")
 def index():
-    return render_template("index.html")
+    title = "Flasker"
+    toppings = ['paneer', 'chicken', 'oregano', 'garlic']
+    return render_template("home.html", 
+                           title = title, 
+                           toppings = toppings)
+
+# Error Pages
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html")
+
+# User Info Page
+@app.route("/user")
+def user():
+    return render_template("user.html")
