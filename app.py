e730c487b0634fcbdea851fef90cb5087800539a
atharkn7 <athar.kn7@gmail.com>
2025-09-08 16:35:46 +0530

    User Form added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Length
 
 # Initializing the app
 app = Flask(__name__)
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = 'pass'
 # User login Form
 class UserForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    email = EmailField('Email', validators=[Email(), DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6, max=16), DataRequired()]) # 6 - 12 digit pass
     submit = SubmitField('Submit')
 
@@ -34,7 +34,23 @@ def page_not_found(e):
     return render_template("500.html")
 
 # User Info Page
-@app.route("/user")
+@app.route("/user", methods=["GET", "POST"])
 def user():
     form = UserForm()
-    return render_template("user.html", form=form)
+    name = None # Assigning None to show form when value none and when name entered by user then it would show the greet message
+
+    # Validating form
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+        return render_template("user.html", 
+                               form=form, 
+                               name=name, 
+                               email=email, 
+                               password=password)
+    
+    else:
+        return render_template("user.html", 
+                               form=form, 
+                               name=name)
