11617ed7609f9d452b3c167cab667f0248dbee00
atharkn7 <athar.kn7@gmail.com>
2025-09-20 16:01:35 +0530

    Pass hashing added to Users Model

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField    # EmailField, PasswordField
 from wtforms.validators import DataRequired, Length
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 # Initializing the app
@@ -24,6 +25,7 @@ class Users(db.Model):
     email = db.Column(db.String(120), nullable=False, unique=True)
     fav_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.now)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     # A way to add a constraint at the db level would need - 
         # from sqlalchemy import CheckConstraint
@@ -32,6 +34,19 @@ class Users(db.Model):
     )
     """
 
+    # Password hashing
+    @property
+    def password(self):
+        raise AttributeError("Password is not a readable attribute!")
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    # Repr
     def __repr__(self):
         return '<Name %r>' % self.name
 
